91c69f22a5e5b7a0244d41a9440178bf2f24a64c
vanderheijden86 <vanderheijden86@gmail.com>
2023-03-18 19:24:02 +0100

    change genesis time to now

--- scripts/build-genesis.sh
+++ scripts/build-genesis.sh
@@ -83,7 +83,6 @@ gen_all_config(){
     echo 'export ITERATION_NUMBER="'"${ITERATION_NUMBER}"'"' >> ./dist/retention.vars
     echo 'export ITERATION_RELEASE="ephemery-'"${ITERATION_NUMBER}"'"' >> ./dist/retention.vars
     echo 'export GENESIS_TIMESTAMP="'"${GENESIS_TIMESTAMP}"'"' >> ./dist/retention.vars
-    echo 'export GENESIS_RESET_INTERVAL="'"${GENESIS_INTERVAL}"'"' >> ./dist/retention.vars
     echo 'export CHAIN_ID="'"${CHAIN_ID}"'"' >> ./dist/retention.vars
     gen_el_config
     gen_cl_config
